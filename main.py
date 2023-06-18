943bcd9e5003a2eca04f8f3a50f782f9214ee31f
Montoito <andremontoito@gmail.com>
2023-06-18 16:44:07 +0100

    versão 1.0.0.3

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from tkinter import *
 from tkinter import ttk
-from PIL import Image, ImageTk
+from PIL import Image, ImageTk #install pillow
 
 
 menu_x = 0
@@ -13,7 +13,7 @@ def gestao_alunos():
     root.resizable(0, 0)
 
     FONT = 'Roboto 12'
-
+    BG = '#545454'
 
 
     def movemenu():
@@ -38,31 +38,75 @@ def gestao_alunos():
 
 
 
-    main_frame = Frame(root, width=800, height=800, bg='#595454')
+    main_frame = Frame(root, width=800, height=800)
     main_frame.pack(expand=True, fill='both')
 
+
+    LEFTFRAME = Frame(main_frame)                  #left frame
+    LEFTFRAME.pack(side='left', padx=50, fill='both')
+
+
+
+    image_person = Frame(LEFTFRAME)
+    image_person.pack(pady=20, side='left', fill='both')
+
+    icon_person_original = Image.open('person_icon.png').resize((200, 200))
+    icon_person_tk = ImageTk.PhotoImage(icon_person_original)
+
+    icon_person = Label(image_person, image=icon_person_tk)
+    icon_person.pack(padx=10)
+
+
+
+
+    info_person = Frame(LEFTFRAME)
+    info_person.pack(pady=20, side='left', fill='both')
+
+    white_space = Label(info_person)
+    white_space.pack(pady=5)
+
+    info_person_name = Label(info_person, text='Nome: André', font='Roboto 18')
+    info_person_name.pack(padx=30, pady=15)
+
+    info_person_email = Label(info_person, text='Email: andremontoito@gmail.com', font='Roboto 18')
+    info_person_email.pack(padx=30, pady=15)
+
+
+
+
+    RIGHTFRAME = Frame(main_frame)                  #rigth frame
+    RIGHTFRAME.pack(side='left', padx=50, fill='both')
+
+
+
+
+
+
+
+
+
     # menu
-    menu_panel = Frame(main_frame, bg='#474343')
+    menu_panel = Frame(main_frame, bg='grey')
     menu_panel.place(x=-210, y=0.01)
 
-    button1 = Button(menu_panel, text='Gestão de Utilizadores', takefocus=False, bg='#383434', borderwidth=0,
-                     fg='white', anchor='w', font=FONT, cursor='hand2')
+    button1 = Button(menu_panel, text='Gestão de Utilizadores', takefocus=False, borderwidth=0,
+                     fg='white', anchor='w', font=FONT, cursor='hand2', bg=BG)
     button1.pack(pady=5, fill='both', anchor='e')
 
-    button2 = Button(menu_panel, text='Gestão de alunos', takefocus=False, bg='#383434', borderwidth=0, fg='white',
-                     anchor='w', font=FONT, cursor='hand2')
+    button2 = Button(menu_panel, text='Gestão de alunos', takefocus=False, borderwidth=0, fg='white',
+                     anchor='w', font=FONT, cursor='hand2', bg=BG)
     button2.pack(pady=5, fill='both')
 
-    button3 = Button(menu_panel, text='Gestão de aulas e horários', takefocus=False, bg='#383434', borderwidth=0,
-                     fg='white', anchor='w', font=FONT, cursor='hand2')
+    button3 = Button(menu_panel, text='Gestão de aulas e horários', takefocus=False, borderwidth=0,
+                     fg='white', anchor='w', font=FONT, cursor='hand2', bg=BG)
     button3.pack(pady=5, fill='both')
 
-    button4 = Button(menu_panel, text='Gestão de pagamentos', takefocus=False, bg='#383434', borderwidth=0, fg='white',
-                     anchor='w', font=FONT, cursor='hand2')
+    button4 = Button(menu_panel, text='Gestão de pagamentos', takefocus=False, borderwidth=0, fg='white',
+                     anchor='w', font=FONT, cursor='hand2', bg=BG)
     button4.pack(pady=5, fill='both')
 
-    button5 = Button(menu_panel, text='Performance de alunos', takefocus=False, bg='#383434', borderwidth=0, fg='white',
-                     anchor='w', font=FONT, cursor='hand2')
+    button5 = Button(menu_panel, text='Performance de alunos', takefocus=False, borderwidth=0, fg='white',
+                     anchor='w', font=FONT, cursor='hand2', bg=BG)
     button5.pack(pady=5, fill='both')
 
     # hambuerguer menu
@@ -72,8 +116,7 @@ def gestao_alunos():
     goBack_original = Image.open('go_back.png').resize((35, 35))
     goBack_tk = ImageTk.PhotoImage(goBack_original)
 
-    hamburguer_button = Button(main_frame, image=hamburguer_tk, takefocus=False, bg='#595454',
-                               activebackground='#595454',
+    hamburguer_button = Button(main_frame, image=hamburguer_tk, takefocus=False,
                                borderwidth=0, cursor='hand2', command=movemenu)
     hamburguer_button.place(x=5, y=5, bordermode='ignore')
 
