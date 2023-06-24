a5f3daff777d89d3b2d85add2c43188cb6e89e4b
Montoito <andremontoito@gmail.com>
2023-06-24 12:32:13 +0100

    versão 1.0.0.9 - atualização com menu novo

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ import database
 import mysql.connector
 from tkinter import messagebox
 
-menu_x = 0
 
 def gestao_alunos():
+
     mydb = mysql.connector.connect(
         host="localhost",
         user="root",
@@ -20,30 +20,31 @@ def gestao_alunos():
     root.geometry('1280x720+280+150')
     root.resizable(0, 0)
 
-    FONT = 'Roboto 12'
-    BG = '#545454'
+    FONT = 'Arial 12'
 
+    button_styles = {
+        'bg': '#008080',
+        'fg': 'white',
+        'activebackground': '#4C4C4C',
+        'activeforeground': 'white',
+        'font': FONT,
+        'borderwidth': 0,
+        'highlightthickness': 0,
+        'relief': 'flat',
+        'cursor': 'hand2',
+    }
 
-    def movemenu():
-        global menu_x
-        menu_x += 0.001
-        menu_panel.place(relx=menu_x, rely=0.095)
-
-        if menu_x < 0.1625:
-            root.after(1, movemenu)
-        hamburguer_button.configure(command=goback)
-        hamburguer_button.configure(image=goBack_tk)
-
-    def goback():
-        global menu_x
-        menu_x -= 0.001
-        menu_panel.place(relx=menu_x, rely=0.095)
-
-        if menu_x > -0.1625:
-            root.after(1, goback)
-        hamburguer_button.configure(command=movemenu)
-        hamburguer_button.configure(image=hamburguer_tk)
-
+    button_styles_mini_menu = {
+        'bg': '#c9c9c9',
+        'fg': 'black',
+        'activebackground': '#4C4C4C',
+        'activeforeground': 'white',
+        'font': FONT,
+        'borderwidth': 0,
+        'highlightthickness': 0,
+        'relief': 'flat',
+        'cursor': 'hand2',
+    }
 
     def person_info():
         person_curso()
@@ -56,8 +57,8 @@ def gestao_alunos():
         for rows in q_alunos:
             if rows[1] == nome_selecionado:
                 aluno_nome_aux = rows[1]
-                aluno_phone_aux = rows[2]
-                aluno_email_aux = rows[3]
+                aluno_email_aux = rows[2]
+                aluno_phone_aux = rows[3]
                 aluno_nome.set(f'Nome: {aluno_nome_aux}')
                 aluno_phone.set(f'Telemóvel: {aluno_phone_aux}')
                 aluno_email.set(f'Email: {aluno_email_aux}')
@@ -101,8 +102,32 @@ def gestao_alunos():
                 pass
         else:
             messagebox.showerror('Erro', 'Selecione um aluno antes de eliminá-lo')
+
+
+
     main_frame = Frame(root, width=800, height=800)
     main_frame.pack(expand=True, fill='both')
+
+
+    menu_frame = Frame(main_frame, bg='#383838', width=200, height=720)
+    menu_frame.pack(side='left', fill='y')
+
+    button1 = Button(menu_frame, text='Gestão de Utilizadores', **button_styles)
+    button1.pack(pady=10, padx=20, fill='x')
+
+    button2 = Button(menu_frame, text='Gestão de Alunos', **button_styles)
+    button2.pack(pady=10, padx=20, fill='x')
+
+    button3 = Button(menu_frame, text='Gestão de Aulas e Horários', **button_styles)
+    button3.pack(pady=10, padx=20, fill='both')
+
+    button4 = Button(menu_frame, text='Gestão de Pagamentos', **button_styles)
+    button4.pack(pady=10, padx=20, fill='x')
+
+    button5 = Button(menu_frame, text='Performance de Alunos', **button_styles)
+    button5.pack(pady=10, padx=20, fill='x')
+
+
 
     LEFTFRAME = Frame(main_frame) #--------------------------------left frame---------------------------------
     LEFTFRAME.pack(side='left', padx=50, fill='both', expand=True)
@@ -119,13 +144,13 @@ def gestao_alunos():
 
     alunos = database.alunos()
 
-    change_person = ttk.Combobox(image_person_frame, values=alunos, font='Roboto 14', justify='center')
+    change_person = ttk.Combobox(image_person_frame, values=alunos, font='RArial 14', justify='center')
     change_person.pack(pady=5)
 
     select_button = ttk.Button(image_person_frame, text='Selecionar aluno acima', command=person_info, width=40)
     select_button.pack(pady=5)
 
-    delete_person = Button(image_person_frame, text='Eliminar Aluno', takefocus=False, cursor='hand2', font='Roboto 12',
+    delete_person = Button(image_person_frame, text='Eliminar Aluno', takefocus=False, cursor='hand2', font='Arial 12',
                            bg='#87232d', fg='white', command=delete_aluno)
     delete_person.pack(pady=10)
 
@@ -147,13 +172,13 @@ def gestao_alunos():
     aluno_phone.set('Telemóvel: ')
     aluno_email.set('Email: ')
 
-    info_person_name = Label(info_person, textvariable=aluno_nome, font='Roboto 16')
+    info_person_name = Label(info_person, textvariable=aluno_nome, font='Arial 16')
     info_person_name.pack(padx=30, pady=15, anchor='w')
 
-    info_person_email = Label(info_person, textvariable=aluno_email, font='Roboto 16')
+    info_person_email = Label(info_person, textvariable=aluno_email, font='Arial 16')
     info_person_email.pack(padx=30, pady=15, anchor='w')
 
-    info_person_phone = Label(info_person, textvariable=aluno_phone, font='Roboto 16')
+    info_person_phone = Label(info_person, textvariable=aluno_phone, font='Arial 16')
     info_person_phone.pack(padx=30, pady=15, anchor='w')
 
     white_space = Label(info_person)
@@ -162,12 +187,12 @@ def gestao_alunos():
 
 
 
-    cursos_text = Label(info_person, text='Cursos Inscritos:', font='Roboto 16')
+    cursos_text = Label(info_person, text='Cursos Inscritos:', font='Arial 16')
     cursos_text.pack(padx=30, pady=15, anchor='w')
 
 
 
-    info_person_courses = Listbox(info_person, font='Roboto 14', width=30, borderwidth=0)
+    info_person_courses = Listbox(info_person, font='Arial 14', width=30, borderwidth=0)
     info_person_courses.pack(padx=30, pady=15, anchor='w')
 
 
@@ -178,61 +203,18 @@ def gestao_alunos():
 
 
 
-    menu_alunos = Frame(RIGHTFRAME)
-    menu_alunos.pack(anchor='ne', expand=True, pady=50)
-
-    perfil_info_button = Button(menu_alunos, text='Informação Pessoal', takefocus=False, cursor='hand2', width=30,
-                                borderwidth=0, font='Roboto 14', anchor='e')
-    perfil_info_button.pack(anchor='e')
-
-    create_aluno_button = Button(menu_alunos, text='Criar Perfil de Aluno', takefocus=False, cursor='hand2', width=30,
-                                 borderwidth=0, font='Roboto 14', anchor='e')
-    create_aluno_button.pack(anchor='e')
-
-    join_curso = Button(menu_alunos, text='Adicionar Aluno a um curso', takefocus=False, cursor='hand2', width=30,
-                        borderwidth=0, font='Roboto 14', anchor='e')
-    join_curso.pack(anchor='e')
+    menu_alunos = Frame(main_frame, bg='#b3b5b4', width=150, height=50)
+    menu_alunos.pack(anchor='ne', expand=True)
 
 
+    button1 = Button(menu_alunos, text='Informação Pessoal', **button_styles_mini_menu)
+    button1.pack(pady=10, padx=10, fill='x')
 
+    button2 = Button(menu_alunos, text='Adicionar Aluno a Curso', **button_styles_mini_menu)
+    button2.pack(pady=10, padx=10, fill='x')
 
-
-
-
-    # menu
-    menu_panel = Frame(main_frame, bg='grey')
-    menu_panel.place(x=-210, y=0.01)
-
-    button1 = Button(menu_panel, text='Gestão de Utilizadores', takefocus=False, borderwidth=0,
-                     fg='white', anchor='w', font=FONT, cursor='hand2', bg=BG)
-    button1.pack(pady=5, fill='both', anchor='e')
-
-    button2 = Button(menu_panel, text='Gestão de alunos', takefocus=False, borderwidth=0, fg='white',
-                     anchor='w', font=FONT, cursor='hand2', bg=BG)
-    button2.pack(pady=5, fill='both')
-
-    button3 = Button(menu_panel, text='Gestão de aulas e horários', takefocus=False, borderwidth=0,
-                     fg='white', anchor='w', font=FONT, cursor='hand2', bg=BG)
-    button3.pack(pady=5, fill='both')
-
-    button4 = Button(menu_panel, text='Gestão de pagamentos', takefocus=False, borderwidth=0, fg='white',
-                     anchor='w', font=FONT, cursor='hand2', bg=BG)
-    button4.pack(pady=5, fill='both')
-
-    button5 = Button(menu_panel, text='Performance de alunos', takefocus=False, borderwidth=0, fg='white',
-                     anchor='w', font=FONT, cursor='hand2', bg=BG)
-    button5.pack(pady=5, fill='both')
-
-    # hambuerguer menu
-    hamburguer_original = Image.open('menu_hamburguer.png').resize((35, 35))
-    hamburguer_tk = ImageTk.PhotoImage(hamburguer_original)
-
-    goBack_original = Image.open('go_back.png').resize((35, 35))
-    goBack_tk = ImageTk.PhotoImage(goBack_original)
-
-    hamburguer_button = Button(main_frame, image=hamburguer_tk, takefocus=False,
-                               borderwidth=0, cursor='hand2', command=movemenu)
-    hamburguer_button.place(x=5, y=5, bordermode='ignore')
+    title_page = Label(main_frame, text='Informação Pessoal', font='Arial 16 bold')
+    title_page.place(x=540, y=5)
 
     root.mainloop()
 
--- tf_prog_av.sql
+++ tf_prog_av.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Jun 23, 2023 at 10:54 AM
+-- Generation Time: Jun 23, 2023 at 05:39 PM
 -- Server version: 8.0.31
 -- PHP Version: 8.0.26
 
@@ -35,6 +35,25 @@ CREATE TABLE IF NOT EXISTS `q_alunos_cursos` (
   KEY `curso_id` (`curso_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
+--
+-- Dumping data for table `q_alunos_cursos`
+--
+
+INSERT INTO `q_alunos_cursos` (`aluno_id`, `curso_id`) VALUES
+(1, 1),
+(1, 2),
+(1, 3),
+(2, 2),
+(2, 4),
+(2, 5),
+(3, 3),
+(3, 4),
+(4, 1),
+(4, 3),
+(5, 2),
+(5, 4),
+(5, 5);
+
 -- --------------------------------------------------------
 
 --
@@ -54,7 +73,23 @@ CREATE TABLE IF NOT EXISTS `q_aulas` (
   PRIMARY KEY (`aula_id`),
   KEY `aulas_curso_id` (`aulas_curso_id`),
   KEY `aula_prof_id` (`aula_prof_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `q_aulas`
+--
+
+INSERT INTO `q_aulas` (`aula_id`, `aulas_curso_id`, `aula_desc`, `aula_prof_id`, `aula_inicio`, `aula_termino`, `aula_sumarios`, `aula_ausencias`) VALUES
+(1, 1, 'Introduction to Algebra', 11, '2023-06-24 00:00:00', '2023-06-24 00:00:00', 'Topics: Linear equations, Quadratic equations', 0),
+(2, 1, 'Introduction to Algebra', 11, '2023-06-24 00:00:00', '2023-06-24 00:00:00', 'Topics: Linear equations, Quadratic equations', 0),
+(3, 2, 'Grammar Rules', 12, '2023-06-24 00:00:00', '2023-06-24 00:00:00', 'Topics: Nouns, Verbs, Adjectives', 0),
+(4, 2, 'Grammar Rules', 12, '2023-06-24 00:00:00', '2023-06-24 00:00:00', 'Topics: Nouns, Verbs, Adjectives', 0),
+(5, 3, 'Chemical Reactions', 13, '2023-06-23 00:00:00', '2023-06-23 00:00:00', 'Topics: Balancing equations, Types of reactions', 0),
+(6, 3, 'Chemical Reactions', 13, '2023-06-23 00:00:00', '2023-06-23 00:00:00', 'Topics: Balancing equations, Types of reactions', 0),
+(7, 4, 'World War I', 14, '2023-06-24 00:00:00', '2023-06-24 00:00:00', 'Topics: Causes, Events, Consequences', 0),
+(8, 4, 'World War I', 14, '2023-06-24 00:00:00', '2023-06-24 00:00:00', 'Topics: Causes, Events, Consequences', 0),
+(9, 5, 'Continents and Oceans', 15, '2023-06-23 00:00:00', '2023-06-23 00:00:00', 'Topics: Names, Locations', 0),
+(10, 5, 'Continents and Oceans', 15, '2023-06-23 00:00:00', '2023-06-23 00:00:00', 'Topics: Names, Locations', 0);
 
 -- --------------------------------------------------------
 
@@ -72,7 +107,23 @@ CREATE TABLE IF NOT EXISTS `q_avaliacoes` (
   PRIMARY KEY (`avaliacao_id`),
   KEY `avaliacao_aluno_id` (`avaliacao_aluno_id`),
   KEY `avaliacao_prof_id` (`avaliacao_prof_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `q_avaliacoes`
+--
+
+INSERT INTO `q_avaliacoes` (`avaliacao_id`, `avaliacao_aluno_id`, `avaliacao_data`, `avaliacao_aula`, `avaliacao_prof_id`) VALUES
+(1, 1, '2023-06-24', 'Introduction to Algebra', 11),
+(2, 2, '2023-06-24', 'Introduction to Algebra', 11),
+(3, 3, '2023-06-24', 'Grammar Rules', 12),
+(4, 4, '2023-06-24', 'Grammar Rules', 12),
+(5, 5, '2023-06-23', 'Chemical Reactions', 13),
+(6, 6, '2023-06-23', 'Chemical Reactions', 13),
+(7, 7, '2023-06-24', 'World War I', 14),
+(8, 8, '2023-06-24', 'World War I', 14),
+(9, 9, '2023-06-23', 'Continents and Oceans', 15),
+(10, 10, '2023-06-23', 'Continents and Oceans', 15);
 
 -- --------------------------------------------------------
 
@@ -86,9 +137,20 @@ CREATE TABLE IF NOT EXISTS `q_cursos` (
   `curso_desc` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `curso_horas` int NOT NULL,
   `curso_preco` int NOT NULL,
-  `curso_alunos_id` int NOT NULL,
   PRIMARY KEY (`curso_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `q_cursos`
+--
+
+INSERT INTO `q_cursos` (`curso_id`, `curso_desc`, `curso_horas`, `curso_preco`) VALUES
+(1, 'Mathematics', 20, 100),
+(2, 'English', 16, 80),
+(3, 'Chemistry', 18, 90),
+(4, 'History', 14, 75),
+(5, 'Geography', 12, 70),
+(6, 'Curso Teste', 400, 600);
 
 -- --------------------------------------------------------
 
@@ -105,7 +167,23 @@ CREATE TABLE IF NOT EXISTS `q_pagamentos` (
   `pagamento_metodo` varchar(100) NOT NULL,
   PRIMARY KEY (`pagamento_id`),
   KEY `pagamento_aluno_id` (`pagamento_aluno_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `q_pagamentos`
+--
+
+INSERT INTO `q_pagamentos` (`pagamento_id`, `pagamento_data`, `pagamento_aluno_id`, `pagamento_valor`, `pagamento_metodo`) VALUES
+(1, '2023-06-24', 1, 100, 'Credit Card'),
+(2, '2023-06-24', 2, 100, 'Credit Card'),
+(3, '2023-06-24', 3, 80, 'PayPal'),
+(4, '2023-06-24', 4, 80, 'PayPal'),
+(5, '2023-06-23', 5, 90, 'Bank Transfer'),
+(6, '2023-06-23', 6, 90, 'Bank Transfer'),
+(7, '2023-06-24', 7, 75, 'Cash'),
+(8, '2023-06-24', 8, 75, 'Cash'),
+(9, '2023-06-23', 9, 70, 'Credit Card'),
+(10, '2023-06-23', 10, 70, 'Credit Card');
 
 -- --------------------------------------------------------
 
@@ -119,7 +197,15 @@ CREATE TABLE IF NOT EXISTS `q_perfis` (
   `perfil_nome` varchar(100) NOT NULL,
   `perfil_estado` int NOT NULL,
   PRIMARY KEY (`perfil_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `q_perfis`
+--
+
+INSERT INTO `q_perfis` (`perfil_id`, `perfil_nome`, `perfil_estado`) VALUES
+(1, 'Student', 1),
+(2, 'Teacher', 1);
 
 -- --------------------------------------------------------
 
@@ -140,7 +226,22 @@ CREATE TABLE IF NOT EXISTS `q_utilizadores` (
   `utilizador_perfil` int NOT NULL,
   PRIMARY KEY (`utilizador_id`),
   KEY `utilizador_perfil` (`utilizador_perfil`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `q_utilizadores`
+--
+
+INSERT INTO `q_utilizadores` (`utilizador_id`, `utilizador_nome`, `utilizador_email`, `utilizador_contacto`, `utilizador_morada`, `utilizador_localidade`, `utilizador_nascimento`, `utilizador_senha`, `utilizador_perfil`) VALUES
+(1, 'John Doe', 'john@example.com', '123456789', '123 Main St', 'City', '2000-01-01', 'password', 1),
+(2, 'Jane Smith', 'jane@example.com', '987654321', '456 Elm St', 'City', '2001-02-02', 'password', 1),
+(3, 'Mike Johnson', 'mike@example.com', '555555555', '789 Oak St', 'City', '1999-03-03', 'password', 1),
+(4, 'Sarah Davis', 'sarah@example.com', '111111111', '321 Pine St', 'City', '1998-04-04', 'password', 1),
+(5, 'Emily Wilson', 'emily@example.com', '999999999', '654 Birch St', 'City', '2002-05-05', 'password', 1),
+(6, 'Professor Johnson', 'professor@example.com', '444444444', '111 Oak St', 'City', '1975-01-01', 'password', 2),
+(7, 'Professor Smith', 'professor2@example.com', '222222222', '222 Elm St', 'City', '1980-02-02', 'password', 2),
+(8, 'Professor Davis', 'professor3@example.com', '333333333', '333 Pine St', 'City', '1985-03-03', 'password', 2),
+(9, 'Professor Wilson', 'professor4@example.com', '666666666', '444 Birch St', 'City', '1990-04-04', 'password', 2);
 
 --
 -- Constraints for dumped tables
